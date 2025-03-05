9ee5788047466eb5c0073b358b3da1117f4cc796
lauechev <laura.echeverri3@gmail.com>
2025-03-05 15:07:19 -0500

    👾 vite and deploy

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  publicDir: 'estaticos',
+  publicDir: 'assets',
   build: {
-    outDir: 'publico',
-    assetsDir: 'estaticos',
+    outDir: 'public',
+    assetsDir: 'assets',
     sourcemap: true,
   },
   css: {
