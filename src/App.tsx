536790337c916e97bd1cb5cee33311b0cb898ac0
lauechev <laura.echeverri3@gmail.com>
2025-03-05 15:07:03 -0500

    🩴 set up for template

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import './scss/styles.scss';
 import React from 'react';
+import { GlobalStyle } from './styles';
 
 const App = () => {
-  return <h1>Hello, Vite + React!</h1>;
+  return (
+    <div className="app">
+      <GlobalStyle />
+      <h1>Hello, Vite + React!</h1>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/styles.ts
@@ -0,0 +1,14 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    color: #fbfbfb;
+    height: 100vh;
+    width: 100vw;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    text-rendering: optimizedLegibility;
+    background-color: #2828e6;
+  }
+`;
