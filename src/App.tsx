52af0c363e4460408eeaa767327ec25dcc8c0149
lauechev <laura.echeverri3@gmail.com>
2025-04-23 18:15:38 -0500

    🪀 calculate cursor distance

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { GlobalStyle, ImageContainer, Wrapper } from './styles';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import ImgBox from './components/ImgBox';
+import { useState } from 'react';
 
 //prettier-ignore
 const matrix = [
@@ -14,12 +15,34 @@ const matrix = [
 ];
 
 const App = () => {
+  const [distance, setDistance] = useState(1);
+
+  const easing = (num: number) => Math.pow(num, 3);
+
+  const calculateDistance = ([x, y]: [number, number]) => {
+    const center = [window.innerWidth / 2, window.innerHeight / 2];
+    const maxHypot = Math.hypot(center[0], center[1]);
+    const hypot = Math.hypot(center[0] - x, center[1] - y);
+    const distance = hypot / maxHypot;
+    const easeDistance = easing(distance);
+    setDistance(easeDistance);
+  };
+
+  const handleMove = ({ clientX, clientY }: React.MouseEvent) => {
+    calculateDistance([clientX, clientY]);
+  };
+
+  const handleTouchMove = ({ touches }: React.TouchEvent) => {
+    calculateDistance([touches[0].clientX, touches[0].clientY]);
+  };
+
+  console.log(distance);
   return (
     <>
       <GlobalStyle />
       <Header />
       <Footer />
-      <Wrapper>
+      <Wrapper onMouseMove={handleMove}>
         <ImageContainer>
           {matrix.map(([x, y], index) => (
             <ImgBox key={index} x={x} y={y} />
